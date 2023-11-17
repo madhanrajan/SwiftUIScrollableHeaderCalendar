e31e5c607cca07fa0b54b6b3a7d3339ee8bbd209
madhanrajan <madhanrajan.varadharajan@gmail.com>
2023-11-18 02:42:46 +0800

    Initial Commit

---
+++ SwiftUIScrollableHeaderCalendar/SwiftUIScrollableHeaderCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollableHeaderCalendarApp.swift
+//  SwiftUIScrollableHeaderCalendar
+//
+//  Created by Madhan on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollableHeaderCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
